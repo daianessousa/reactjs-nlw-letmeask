0a017a16720b355a0ab94c7d3cbca627e5e9825c
daianessousa <daisousa.c@gmail.com>
2021-06-21 22:06:00 -0300

    routes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { useHistory } from 'react-router'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
@@ -5,6 +7,12 @@ import '../styles/auth.scss'
 import { Button } from '../components/Button';
 
 export function Home() {
+  const history = useHistory();
+
+  function navigateToNewRoom() {
+    history.push('/rooms/new')
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -15,20 +23,20 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room"> 
-            <img src={googleIconImg} alt="Logo do Google"/>
-            Crie sua sala com o Google 
-            </button>
-            <div className="separator"> Ou entre em uma Sala </div>
-            <form>
-              <input 
-                type="text"
-                placeholder="Digite o código da sala"
-              />
-              <Button type="submit">
-                Entrar na sala
-              </Button>
-            </form>
+          <button onClick={navigateToNewRoom} className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div className="separator"> Ou entre em uma sala </div>
+          <form>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+            />
+            <Button type="submit">
+              Entrar na sala
+            </Button>
+          </form>
         </div>
       </main>
     </div>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
@@ -16,18 +18,18 @@ export function NewRoom() {
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
           <h2> Criar uma nova Sala </h2>
-            <form>
-              <input 
-                type="text"
-                placeholder="Nome da Sala"
-              />
-              <Button type="submit">
-                Criar sala
-              </Button>
-            </form>
-            <p> Quer entrar em uma sala existente? 
-              <a href="#">Clique aqui</a>
-            </p>
+          <form>
+            <input
+              type="text"
+              placeholder="Nome da Sala"
+            />
+            <Button type="submit">
+              Criar sala
+            </Button>
+          </form>
+          <p> Quer entrar em uma sala existente?
+            <Link to="/">Clique aqui</Link>
+          </p>
         </div>
       </main>
     </div>
